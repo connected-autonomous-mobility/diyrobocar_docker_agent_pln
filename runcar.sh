0ec34acf7520f971cde513d9d637a697f6a18008
Rainer Bareiß <Heavy02011@users.noreply.github.com>
2021-07-30 06:50:04 +0200

    Update runcar.sh

--- runcar.sh
+++ runcar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run pln model using the docker gpu image (adapted from Toni's script)
 
-cmd_line="python3 /root/mycar/manage.py drive --model=/root/mycar/models/mymodel.h5"
+cmd_line="python3 /root/myrace/manage.py drive --model=/root/myrace/models/mymodel.h5"
 
 
 echo "Executing in the docker (gpu image):"
@@ -9,4 +9,4 @@ echo $cmd_line
 
 docker run -it --rm --network host \
   parkinglotnerds/diyrobocars:race_9\
-  bash -c "cd /root/mycar/ && $cmd_line"
+  bash -c "cd /root/myrace/ && $cmd_line"
