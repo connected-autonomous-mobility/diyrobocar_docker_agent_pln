30f1abe3f45e473dbf8fa089e4a82b0dd26cae03
heavy02011 <heavy02011>
2021-09-10 12:42:07 +0200

    feat: ros v2 docker ADDED

---
+++ ros/installROS1_pln.sh
@@ -0,0 +1,3 @@
+
+./installROS.sh -p ros-melodic-desktop-full -p rqt_rviz -p rviz_plugin_tutorials -p librviz_tutorial
+
---
+++ ros/start_pln_ros_v2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose -f ./pln-docker-compose_ros_v2.yml up --build
+
